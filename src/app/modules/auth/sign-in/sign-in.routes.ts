009496c379a5e3e8d8a953c4aa5b0c5fc4f86ddb
leningael <leningaelrs@gmail.com>
2024-05-09 11:50:09 -0600

    feat: :tada: Initial commit with base project
    Template for vehicles admin web

---
+++ src/app/modules/auth/sign-in/sign-in.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { SignInClassicComponent } from 'app/modules/auth/sign-in/sign-in.component';
+
+export default [
+    {
+        path     : '',
+        component: SignInClassicComponent,
+    },
+] as Routes;
---
+++ src/app/modules/auth/sign-up/sign-up.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { SignUpClassicComponent } from 'app/modules/auth/sign-up/sign-up.component';
+
+export default [
+    {
+        path     : '',
+        component: SignUpClassicComponent,
+    },
+] as Routes;
