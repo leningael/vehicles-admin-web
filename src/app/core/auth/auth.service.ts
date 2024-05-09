121ed43b2a977675613bfe990db80e676fdc83fd
leningael <leningaelrs@gmail.com>
2024-05-09 14:45:56 -0600

    feat: :sparkles: Sign in flow added - #36

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,11 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { AuthUtils } from 'app/core/auth/auth.utils';
 import { UserService } from 'app/core/user/user.service';
+import { environment } from 'environments/environment';
 import { catchError, Observable, of, switchMap, throwError } from 'rxjs';
+import { SignInResponse } from './interfaces/auth.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class AuthService
 {
+    readonly apiUrl: string = environment.apiUrl;
     private _authenticated: boolean = false;
     private _httpClient = inject(HttpClient);
     private _userService = inject(UserService);
@@ -37,68 +40,30 @@ export class AuthService
      *
      * @param credentials
      */
-    signIn(credentials: { email: string; password: string }): Observable<any>
+    signIn(credentials: { email: string; password: string }): Observable<SignInResponse>
     {
         // Throw error, if the user is already logged in
         if ( this._authenticated )
         {
-            return throwError('User is already logged in.');
+            return throwError(() => 'User is already logged in.');
         }
-
-        return this._httpClient.post('api/auth/sign-in', credentials).pipe(
-            switchMap((response: any) =>
+        const signInFormData = new FormData();
+        signInFormData.append('username', credentials.email);
+        signInFormData.append('password', credentials.password);
+        return this._httpClient.post<SignInResponse>(`${this.apiUrl}/auth/login`, signInFormData).pipe(
+            switchMap((response: SignInResponse) =>
             {
                 // Store the access token in the local storage
-                this.accessToken = response.accessToken;
+                this.accessToken = response.access_token;
 
                 // Set the authenticated flag to true
                 this._authenticated = true;
 
                 // Store the user on the user service
-                this._userService.user = response.user;
+                // this._userService.user = response.user;
 
                 // Return a new observable with the response
                 return of(response);
-            }),
-        );
-    }
-
-    /**
-     * Sign in using the access token
-     */
-    signInUsingToken(): Observable<any>
-    {
-        // Sign in using the token
-        return this._httpClient.post('api/auth/sign-in-with-token', {
-            accessToken: this.accessToken,
-        }).pipe(
-            catchError(() =>
-
-                // Return false
-                of(false),
-            ),
-            switchMap((response: any) =>
-            {
-                // Replace the access token with the new one if it's available on
-                // the response object.
-                //
-                // This is an added optional step for better security. Once you sign
-                // in using the token, you should generate a new one on the server
-                // side and attach it to the response object. Then the following
-                // piece of code can replace the token with the refreshed one.
-                if ( response.accessToken )
-                {
-                    this.accessToken = response.accessToken;
-                }
-
-                // Set the authenticated flag to true
-                this._authenticated = true;
-
-                // Store the user on the user service
-                this._userService.user = response.user;
-
-                // Return true
-                return of(true);
             }),
         );
     }
@@ -152,6 +117,7 @@ export class AuthService
         }
 
         // If the access token exists, and it didn't expire, sign in using it
-        return this.signInUsingToken();
+        // return this.signInUsingToken();
+        return of(true);
     }
 }
---
+++ src/app/core/auth/interfaces/auth.interfaces.ts
@@ -0,0 +1,4 @@
+export interface SignInResponse {
+    access_token: string;
+    token_type: string;
+}
